5ee8466f920913f536fe5031d6a03c200a658834
Baraffe Robin <robin.baraffe@gmail.com>
2020-11-20 11:12:06 +0100

    Add civility & additional street info to user model (#28)

--- models/user.go
+++ models/user.go
@@ -8,29 +8,31 @@ import (
 
 // User struct contain all the user data
 type User struct {
-	ID                      uuid.UUID                `json:"id,omitempty" pg:"id" swaggerignore:"true"`
-	FirstName               string                   `json:"first_name" example:"Henri"`
-	LastName                string                   `json:"last_name" example:"Martin"`
-	Email                   string                   `json:"email" example:"henri.martin@qimpl.fr"`
-	MobilePhoneNumber       string                   `json:"mobile_phone_number" example:"0610890978"`
-	Birthdate               time.Time                `json:"birthdate" example:"1999-10-05"`
-	Country                 string                   `json:"country" example:"FR"`
-	State                   string                   `json:"state" example:"Haut-De-France"`
-	Street                  string                   `json:"street" example:"Rue des peupliers"`
-	City                    string                   `json:"city" example:"Lille"`
-	Zip                     string                   `json:"zip" example:"59000"`
-	IsOwner                 bool                     `json:"is_owner,omitempty" pg:",use_zero" example:"true"`
-	Password                string                   `json:"password,omitempty" example:"MyPassword"`
-	IsEnabled               bool                     `json:"is_enabled,omitempty" pg:",use_zero" example:"true"`
-	IsAdmin                 bool                     `json:"is_admin,omitempty" pg:",use_zero" example:"true"`
-	IsVerified              bool                     `json:"is_verified,omitempty" pg:",use_zero" example:"true"`
-	IsDeleted               bool                     `json:"is_deleted,omitempty" pg:",use_zero" example:"true"`
-	NotificationPreferences *NotificationPreferences `json:"notification_preferences,omitempty" pg:"rel:belongs-to"`
-	StripeCustomerID        string                   `json:"stripe_customer_id,omitempty" pg:"stripe_customer_id" example:"cus_IOwdRp9gIlOjTD"`
-	StripePaymentMethodID   string                   `json:"stripe_payment_method_id,omitempty" pg:"stripe_payment_method_id" example:"pm_1Ho8k8CMhQMU3AqAKJwPYAXj"`
-	CreatedAt               time.Time                `json:"created_at,omitempty" swaggerignore:"true"`
-	UpdatedAt               time.Time                `json:"updated_at,omitempty" swaggerignore:"true"`
-	DeletedAt               time.Time                `json:"deleted_at,omitempty" swaggerignore:"true"`
+	ID                          uuid.UUID                `json:"id,omitempty" pg:"id" swaggerignore:"true"`
+	Civility                    string                   `json:"civility" example:"Mme"`
+	FirstName                   string                   `json:"first_name" example:"Henri"`
+	LastName                    string                   `json:"last_name" example:"Martin"`
+	Email                       string                   `json:"email" example:"henri.martin@qimpl.fr"`
+	MobilePhoneNumber           string                   `json:"mobile_phone_number" example:"0610890978"`
+	Birthdate                   time.Time                `json:"birthdate" example:"1999-10-05"`
+	Country                     string                   `json:"country" example:"FR"`
+	State                       string                   `json:"state" example:"Haut-De-France"`
+	Street                      string                   `json:"street" example:"Rue des peupliers"`
+	AdditionalStreetInformation string                   `json:"additional_street_information,omitempty" example:"Residence des peupliers"`
+	City                        string                   `json:"city" example:"Lille"`
+	Zip                         string                   `json:"zip" example:"59000"`
+	IsOwner                     bool                     `json:"is_owner,omitempty" pg:",use_zero" example:"true"`
+	Password                    string                   `json:"password,omitempty" example:"MyPassword"`
+	IsEnabled                   bool                     `json:"is_enabled,omitempty" pg:",use_zero" example:"true"`
+	IsAdmin                     bool                     `json:"is_admin,omitempty" pg:",use_zero" example:"true"`
+	IsVerified                  bool                     `json:"is_verified,omitempty" pg:",use_zero" example:"true"`
+	IsDeleted                   bool                     `json:"is_deleted,omitempty" pg:",use_zero" example:"true"`
+	NotificationPreferences     *NotificationPreferences `json:"notification_preferences,omitempty" pg:"rel:belongs-to"`
+	StripeCustomerID            string                   `json:"stripe_customer_id,omitempty" pg:"stripe_customer_id" example:"cus_IOwdRp9gIlOjTD"`
+	StripePaymentMethodID       string                   `json:"stripe_payment_method_id,omitempty" pg:"stripe_payment_method_id" example:"pm_1Ho8k8CMhQMU3AqAKJwPYAXj"`
+	CreatedAt                   time.Time                `json:"created_at,omitempty" swaggerignore:"true"`
+	UpdatedAt                   time.Time                `json:"updated_at,omitempty" swaggerignore:"true"`
+	DeletedAt                   time.Time                `json:"deleted_at,omitempty" swaggerignore:"true"`
 }
 
 //UserLogin contain only user credentials used to authentication
